f8bf5048691e8958ae66179c766b23ac6f2e44bf
VyayKumar <49907198+DavidusRidero@users.noreply.github.com>
2026-09-18 18:10:15 +0530

    fix: Minor changes. First try.

--- Q/Q11-20/Q11/Q11.c
+++ Q/Q11-20/Q11/Q11.c
@@ -17,54 +17,54 @@ int main(int argc, char *argv[]) {
         printf("Error code: %d", errno);
         return 1;
     }
-    printf("File Descriptor: %d\n", fd);
+    printf("File Descriptor: \t\t%d\n", fd);
 
     // TODO: Duplicate the file descriptor using dup().
     const int fd_d1 = dup(fd);
     if (fd_d1 < 0) {
         perror("dup() failed");
-        printf("Error code: %d", errno);
+        printf("Error code: %d\n", errno);
         return 1;
     }
-    printf("File Descriptor, by dup(): %d\n", fd_d1);
+    printf("File Descriptor, by dup(): \t%d\n", fd_d1);
 
     // TODO: Duplicate the original descriptor to a specific target number using dup2().
     const int target = 114;
-    printf("Target FD chosen: %d\n", target);
+    printf("Target FD chosen: \t\t%d\n", target);
 
     const int fd_d2 = dup2(fd, target);
     if (fd_d2 < 0) {
         perror("dup2() failed");
-        printf("Error code: %d", errno);
+        printf("Error code: %d\n", errno);
         return 1;
     }
-    printf("File Descriptor, by dup2(): %d\n", fd_d2);
+    printf("File Descriptor, by dup2(): \t%d\n", fd_d2);
 
     // TODO: Write text using the original descriptor.
-    char word1[3] = "VIJ";
+    char word1[] = "VIJ";
 
-    int write1 = write(fd, word1, 3);
+    ssize_t write1 = write(fd, word1, 3);
     if (write1 < 0) {
         perror("write() failed");
-        printf("Error code: %d", errno);
+        printf("Error code: %d\n", errno);
         return 1;
     }
 
     // TODO: Write text using the dup() descriptor.
-    char word2[5] = "AY KU";
-    int write2 = write(fd_d1, word2, 5);
+    char word2[] = "AY KU";
+    ssize_t write2 = write(fd_d1, word2, 5);
     if (write2 < 0) {
         perror("write() failed");
-        printf("Error code: %d", errno);
+        printf("Error code: %d\n", errno);
         return 1;
     }
 
     // TODO: Write text using the dup2() descriptor.
-    char word3[3] = "MAR";
-    int write3 = write(fd_d2, word1, 3);
+    char word3[] = "MAR";
+    ssize_t write3 = write(fd_d2, word3, 3);
     if (write3 < 0) {
         perror("write() failed");
-        printf("Error code: %d", errno);
+        printf("Error code: %d\n", errno);
         return 1;
     }
 
