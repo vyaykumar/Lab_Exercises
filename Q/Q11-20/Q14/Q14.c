faa7594fe7165d33028c54196797d19cb1d1437b
VyayKumar <49907198+DavidusRidero@users.noreply.github.com>
2026-09-18 23:21:11 +0530

    [Q14] done : Cleaned and corrected.

--- Q/Q11-20/Q14/Q14.c
+++ Q/Q11-20/Q14/Q14.c
@@ -4,46 +4,32 @@
 #include <errno.h>
 
 int main(int argc, char *argv[]) {
-    // TODO: Verify that argc equals 2 (program name and target file path).
     if (argc != 2) {
-        printf("Usage: %s <source>", argv[0]);
+        printf("Usage: %s <source>\n", argv[0]);
         return 1;
     }
 
-    // TODO: Declare a struct stat instance.
     struct stat sb;
 
-    // TODO: Call lstat(argv[1], &statbuf) to inspect the file without following symlinks.
     int result = lstat(argv[1], &sb);
 
-    // TODO: Check for errors from lstat.
     if (result < 0) {
         perror("lstat() failed");
         return 1;
     }
 
-    // TODO: Evaluate statbuf.st_mode using the S_IS* macros:
-    //       - S_ISREG(): Regular file
-    //       - S_ISDIR(): Directory
-    //       - S_ISCHR(): Character device
-    //       - S_ISBLK(): Block device
-    //       - S_ISFIFO(): FIFO / named pipe
-    //       - S_ISLNK(): Symbolic link
-    //       - S_ISSOCK(): Socket
-
     char *type = "Unknown";
 
-    if (S_ISREG(sb.st_mode)) type = "Regular File";
-    else if (S_ISDIR(sb.st_mode)) type = "Directory";
-    else if (S_ISCHR(sb.st_mode)) type = "Character Device";
-    else if (S_ISBLK(sb.st_mode)) type = "Block Device";
-    else if (S_ISFIFO(sb.st_mode)) type = "FIFO/Pipe";
-    else if (S_ISLNK(sb.st_mode)) type = "Symlink";
-    else if (S_ISSOCK(sb.st_mode)) type = "Socket";
+    if (S_ISREG(sb.st_mode))        type = "Regular File";
+    else if (S_ISDIR(sb.st_mode))   type = "Directory";
+    else if (S_ISCHR(sb.st_mode))   type = "Character Device";  // Use /dev/null as argument.
+    else if (S_ISBLK(sb.st_mode))   type = "Block Device";      // Use /dev/sda  as argument.
+    else if (S_ISFIFO(sb.st_mode))  type = "FIFO/Pipe";         // Can't make one for some reason.
+    else if (S_ISLNK(sb.st_mode))   type = "Symlink";
+    else if (S_ISSOCK(sb.st_mode))  type = "Socket";            // Use /run/systemd/private.
 
-    // TODO: Print the identified file type.
-    printf("File: %s", argv[1]);
-    printf("Type: %s", type);
+    printf("File: %s\n", argv[1]);
+    printf("Type: %s\n", type);
 
     return 0;
 }
