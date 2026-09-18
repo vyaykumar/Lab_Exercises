a02d8140cb6d4024f1a3664cd493063372fe28d9
VyayKumar <49907198+DavidusRidero@users.noreply.github.com>
2026-09-18 18:54:37 +0530

    feat: Implemented. Got help at access mode.

---
+++ Q/Q11-20/Q12/Q12.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+
+int main(int argc, char *argv[]) {
+    // TODO: Verify that argc equals 2 (program name and file path).
+    if (argc != 2) {
+        printf ("Usage: <source>\n");
+        return 1;
+    }
+
+    // TODO: Open the file using custom flags (e.g., O_RDWR | O_APPEND).
+    const int fd = open(argv[1], O_RDWR | O_APPEND);
+    if (fd < 0) {
+        perror("open() failed");
+        printf("Error code: %d", errno);
+        return 1;
+    }
+    printf("File Descriptor: \t\t%d\n", fd);
+
+    // TODO: Use fcntl(fd, F_GETFL) to retrieve the file status flags.
+    auto status = fcntl(fd, F_GETFL);
+    if (status == 1) {
+        perror("fcntl failed");
+        return 1;
+    }
+
+    // TODO: Extract the access mode using (flags & O_ACCMODE).
+    int access_mode = status & O_ACCMODE;
+
+    // TODO: Compare the result against O_RDONLY, O_WRONLY, and O_RDWR and print the mode.
+    switch (access_mode) {
+        case O_RDONLY : printf("Access Mode: Read Only (O_RDONLY).\n");  break;
+        case O_WRONLY : printf("Access Mode: Write Only (O_WRONLY).\n"); break;
+        case O_RDWR :   printf("Access Mode: Read Write (O_RDWR).\n");   break;
+        default : printf("Access Mode: Unknown.\n"); break;
+    }
+
+    // TODO: Check for optional flags like O_APPEND, O_NONBLOCK, and print their presence.
+    if (access_mode & O_APPEND) printf("Flag: O_APPEND is set.\n");
+    if (access_mode & O_NONBLOCK) printf("Flag: O_NONBLOCK is set.\n");
+
+    // TODO: Close the file descriptor.
+    close(fd);
+
+    return 0;
+}
