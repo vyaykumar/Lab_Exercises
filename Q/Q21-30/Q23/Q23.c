ee5496c29ca8e4efa8c8aca883b5d2274b97d0a2
VyayKumar <49907198+DavidusRidero@users.noreply.github.com>
2026-09-19 13:15:46 +0530

    [Q23] chore: cleaned.

--- Q/Q21-30/Q23/Q23.c
+++ Q/Q21-30/Q23/Q23.c
@@ -3,22 +3,17 @@
 #include <sys/types.h>
 
 int main(void) {
-    // TODO: Call fork().
+
     pid_t pid = fork();
     if (pid < 0) {
         perror("fork() failed");
         return 1;
     }
 
-    // TODO: In the child process:
-    //       - Terminate immediately using _exit(0).
     if (pid == 0) {
         _exit(0);
     }
 
-    // TODO: In the parent process:
-    //       - Sleep using sleep(30) or pause() to keep the parent alive.
-    //       - Do not call wait() or waitpid().
     if (pid != 0) {
         printf("Parent PID: %d\n", getpid());
         printf("Child (zombie) PID: %d\n", pid);
