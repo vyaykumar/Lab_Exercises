a675448ed8d3791af655daf4b9f1c9031175583a
VyayKumar <49907198+DavidusRidero@users.noreply.github.com>
2026-09-19 10:01:39 +0530

    [Q19] chore: Cleaned.

--- Q/Q11-20/Q19/Q19.c
+++ Q/Q11-20/Q19/Q19.c
@@ -4,30 +4,23 @@
 
 
 int main(void) {
-    // TODO: Declare struct timespec instances for start and end timestamps.
     struct timespec start, end;
 
-    // TODO: Record start time using clock_gettime(CLOCK_MONOTONIC, &start).
     if (clock_gettime(CLOCK_MONOTONIC, &start) < 0) {
         perror("clock_gettime(start) failed");
         return 1;
     }
 
-    // TODO: Execute getpid().
     int pid = getpid();
 
-    // TODO: Record end time using clock_gettime(CLOCK_MONOTONIC, &end).
     if (clock_gettime(CLOCK_MONOTONIC, &end) < 0) {
         perror ("clock_gettime(end) failed");
         return 1;
     }
 
-    // TODO: Compute elapsed time in nanoseconds:
-    //       (end.tv_sec - start.tv_sec) * 1e9 + (end.tv_nsec - start.tv_nsec).
     unsigned long long elapse = (long long) (end.tv_sec - start.tv_sec)* 1000000000ULL +
                                 (long long) (end.tv_nsec-start.tv_nsec);
 
-    // TODO: Print the elapsed time.
     printf("Time elapsed for getting PID %d: %llu\n", pid, elapse);
 
     return 0;
