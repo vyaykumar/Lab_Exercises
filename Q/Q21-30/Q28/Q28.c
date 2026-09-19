be2f58c0f2d96eb6b83f2a36fa1ae38e396ee133
VyayKumar <49907198+DavidusRidero@users.noreply.github.com>
2026-09-19 14:20:42 +0530

    [Q28] feat: miplemented

--- Q/Q21-30/Q28/Q28.c
+++ Q/Q21-30/Q28/Q28.c
@@ -3,14 +3,18 @@
 
 int main(void) {
     // TODO: Get current scheduling policy using sched_getscheduler(0).
-
+    int sched_res = sched_getscheduler(0);
     // TODO: Declare and set struct sched_param.
     //       - param.sched_priority = sched_get_priority_max(SCHED_FIFO);
+    struct sched_param sp;
+    sp.sched_priority = sched_get_priority_max(SCHED_FIFO);
 
     // TODO: Change policy to SCHED_FIFO or SCHED_RR using:
     //       sched_setscheduler(0, SCHED_FIFO, &param).
+    sched_res = sched_setscheduler(0, SCHED_FIFO, &sp);
 
     // TODO: Verify the updated policy using sched_getscheduler(0).
-
+    if (sched_getscheduler(0) == 1)
+        write(STDOUT_FILENO, "works", 5);
     return 0;
 }
