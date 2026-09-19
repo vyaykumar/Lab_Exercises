ff838e64ccedcbf0e23e5a0b890dac2f837fbddc
VyayKumar <49907198+DavidusRidero@users.noreply.github.com>
2026-09-19 13:58:29 +0530

    [Q26] chore: Works. and cleaned.

--- Q/Q21-30/Q26/Q26.c
+++ Q/Q21-30/Q26/Q26.c
@@ -1,18 +1,12 @@
 #include <unistd.h>
 
 int main(void) {
-    // TODO: Define the argument array:
-    //       char *const args[] = {"/bin/ls", "-l", NULL};
     char *const args[] = {"/bin/ls", "-l", NULL};
 
-    // TODO: Define the environment array:
-    //       char *const envp[] = {NULL};
     char *const envp[] = {NULL};
 
-    // TODO: Invoke execve("/bin/ls", args, envp).
     execve("/bin/ls", args, envp);
 
-    // TODO: If execve returns, handle the error using write() or _exit(1).
     write(STDERR_FILENO, "execve failed\n", 14);
     _exit(1);
 }
