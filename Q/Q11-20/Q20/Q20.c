4782f70d865412986c4152a92bd9f6da6776b5af
VyayKumar <49907198+DavidusRidero@users.noreply.github.com>
2026-09-19 10:09:13 +0530

    [Q20] feat: Corrections.

--- Q/Q11-20/Q20/Q20.c
+++ Q/Q11-20/Q20/Q20.c
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <sys/resource.h>
 #include <errno.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[]) {
     // TODO: Clear errno before calling getpriority().
@@ -15,12 +16,12 @@ int main(int argc, char *argv[]) {
         perror ("getpriority() failed");
         return 1;
     }
-    printf ("Current priority: %d", priority);
+    printf ("Current priority: %d\n", priority);
 
     // TODO: Check if an argument was provided to modify priority; if so, parse it.
     int new_priority = 15;
     if (argc == 2) {
-        new_priority = (int)argv[1];
+        new_priority = atoi(argv[1]);
     }
 
     // TODO: Alter process priority using nice(increment) or setpriority(PRIO_PROCESS, 0, new_nice).
@@ -31,13 +32,14 @@ int main(int argc, char *argv[]) {
     }
 
     // TODO: Retrieve the updated priority with getpriority() and verify the change.
+    errno = 0;
     priority = getpriority(PRIO_PROCESS, 0);
     // TODO: Print the current niceness value.
     if (priority == -1 && errno != 0) {
         perror ("getpriority() failed");
         return 1;
     }
-    printf ("New priority: %d", priority);
+    printf ("New priority: %d\n", priority);
 
     return 0;
 }
