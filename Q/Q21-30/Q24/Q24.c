e5561a76f1de8e170d02f834713663ec9a6d4f0c
VyayKumar <49907198+DavidusRidero@users.noreply.github.com>
2026-09-19 13:20:48 +0530

    [Q24] chore: Cleaned.

--- Q/Q21-30/Q24/Q24.c
+++ Q/Q21-30/Q24/Q24.c
@@ -3,28 +3,22 @@
 #include <sys/types.h>
 
 int main(void) {
-    // TODO: Call fork().
     pid_t pid = fork();
     if (pid < 0) {
         perror("fork() failed");
         return 1;
     }
 
-    // TODO: In the parent process:
-    //       - Print the parent PID using getpid().
-    //       - Terminate immediately using _exit(0).
     if (pid != 0) {
-        printf("Parent PID: %d\n", getpid());
+        printf("[Parent] PID: %d\n", getpid());
+        fflush(stdout);
         _exit(0);
     }
-    // TODO: In the child process:
-    //       - Print the original parent PID using getppid().
-    //       - Sleep using sleep(5) to guarantee parent terminates first.
-    //       - Retrieve and print the new parent PID using getppid() to verify adoption.
+
     if (pid == 0) {
-        printf("Parent PID: %d\n", getppid());
+        printf("[Child] Parent PID: %d\n", getppid()); fflush(stdout);
         sleep(5);
-        printf("Parent PID: %d\n", getppid());
+        printf("[Child] Parent PID: %d\n", getppid()); fflush(stdout);
     }
     return 0;
 }
