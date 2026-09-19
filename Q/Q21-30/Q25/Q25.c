14264e3ae4cc88f86d390303158abe8025d5afbf
VyayKumar <49907198+DavidusRidero@users.noreply.github.com>
2026-09-19 13:35:21 +0530

    [Q25] chore: cleaned.

--- Q/Q21-30/Q25/Q25.c
+++ Q/Q21-30/Q25/Q25.c
@@ -7,9 +7,6 @@ int main(void) {
     pid_t pids[3];
     int sleeps[3] = {2,5,8};
 
-    // TODO: Create child 1 using fork().
-    // TODO: Create child 2 using fork().
-    // TODO: Create child 3 using fork().
     for (size_t idx = 0; idx < 3; idx++) {
         pids[idx] = fork();
         if (pids[idx] < 0) {
@@ -18,9 +15,6 @@ int main(void) {
         }
 
         if (pids[idx] == 0) {
-        // TODO: In each child:
-        //       - Sleep for different intervals (e.g., child 1 sleeps 2s, child 2 sleeps 5s).
-        //       - Exit using _exit(0).
             printf("Child %lu (PID: %d) started. Sleeping for %ds...\n",idx + 1, getpid(), sleeps[idx]);
             sleep(sleeps[idx]);
             printf("Child %lu (PID: %d) exiting.\n", idx + 1, getpid());
@@ -28,9 +22,6 @@ int main(void) {
         }
     }
 
-    // TODO: In the parent process:
-    //       - Call waitpid(target_pid, &status, 0) targeting child 2 specifically.
-    //       - Inspect status using WIFEXITED(status) and WEXITSTATUS(status).
     pid_t target_pid = pids[1];
     int status;
     printf("\nParent waiting for Child 2 (PID: %d) only...\n\n", target_pid);
