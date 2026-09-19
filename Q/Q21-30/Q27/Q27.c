b4e2499ede28b578a2dbaf39b448a907f5033eb5
VyayKumar <49907198+DavidusRidero@users.noreply.github.com>
2026-09-19 14:01:01 +0530

    [Q27] feat: implemented.

--- Q/Q21-30/Q27/Q27.c
+++ Q/Q21-30/Q27/Q27.c
@@ -2,12 +2,15 @@
 
 int main(int argc, char *argv[]) {
     // TODO: Demonstrate execl("/bin/ls", "ls", "-Rl", NULL).
+    execl("/bin/ls", "ls", "-Rl", NULL);
 
     // TODO: Demonstrate execlp("ls", "ls", "-Rl", NULL).
+    execlp("ls", "ls", "-Rl", NULL);
 
-    // TODO: Demonstrate execv("/bin/ls", argv).
+    // TODO: Demonstrate execv("/bin/ls", argv)
+    execv("/bin/ls", argv);
 
     // TODO: Handle failure if exec returns.
-
-    return 0;
+    write(STDOUT_FILENO, "exec() failed", 13);
+    _exit(1);
 }
