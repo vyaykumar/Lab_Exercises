f928905c44e6d96a144061eba001e937ca595e5f
VyayKumar <49907198+DavidusRidero@users.noreply.github.com>
2026-09-19 11:41:34 +0530

    [Q21] chore: done

--- Q/Q21-30/Q21/Q21.c
+++ Q/Q21-30/Q21/Q21.c
@@ -3,19 +3,12 @@
 #include <sys/types.h>
 
 int main(void) {
-    // TODO: Invoke the fork() system call.
     pid_t child = fork();
 
-    // TODO: Verify fork() return value for error handling (-1).
     if (child == -1) {
         perror("fork() failed");
         return 1;
     }
-
-    // TODO: In the child branch (return value == 0):
-    //       - Retrieve child PID using getpid().
-    //       - Retrieve parent PID using getppid().
-    //       - Write details to STDOUT_FILENO using write().
 
     if (child == 0) {
         pid_t pid = getpid();
@@ -29,10 +22,6 @@ int main(void) {
         }
     }
 
-    // TODO: In the parent branch (return value > 0):
-    //       - Retrieve parent PID using getpid().
-    //       - Retrieve child PID from fork return value.
-    //       - Write details to STDOUT_FILENO using write().
     else {
         pid_t pid = getpid();
         pid_t cid = child;
