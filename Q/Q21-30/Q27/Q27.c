161eda36bec1e3827dd88d16ec5c1c4263b08c58
VyayKumar <49907198+DavidusRidero@users.noreply.github.com>
2026-09-19 13:59:40 +0530

    [Q27] chore: skeleton.

---
+++ Q/Q21-30/Q27/Q27.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+    // TODO: Demonstrate execl("/bin/ls", "ls", "-Rl", NULL).
+
+    // TODO: Demonstrate execlp("ls", "ls", "-Rl", NULL).
+
+    // TODO: Demonstrate execv("/bin/ls", argv).
+
+    // TODO: Handle failure if exec returns.
+
+    return 0;
+}
