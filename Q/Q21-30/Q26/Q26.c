a065902ecadab08f41b319205ca327edf4c3a4c8
VyayKumar <49907198+DavidusRidero@users.noreply.github.com>
2026-09-19 13:35:51 +0530

    [Q26] chore: Skelleton.

---
+++ Q/Q21-30/Q26/Q26.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+int main(void) {
+    // TODO: Define the argument array:
+    //       char *const args[] = {"/bin/ls", "-l", NULL};
+
+    // TODO: Define the environment array:
+    //       char *const envp[] = {NULL};
+
+    // TODO: Invoke execve("/bin/ls", args, envp).
+
+    // TODO: If execve returns, handle the error using write() or _exit(1).
+
+    return 0;
+}
