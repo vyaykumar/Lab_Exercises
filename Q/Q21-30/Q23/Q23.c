3ad2b873df9eb074281e22f63baf7e7f272f3dd9
VyayKumar <49907198+DavidusRidero@users.noreply.github.com>
2026-09-19 11:55:05 +0530

    [Q23] feat: Skelton.

---
+++ Q/Q21-30/Q23/Q23.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include <sys/types.h>
+
+int main(void) {
+    // TODO: Call fork().
+
+    // TODO: In the child process:
+    //       - Terminate immediately using _exit(0).
+
+    // TODO: In the parent process:
+    //       - Sleep using sleep(30) or pause() to keep the parent alive.
+    //       - Do not call wait() or waitpid().
+
+    return 0;
+}
