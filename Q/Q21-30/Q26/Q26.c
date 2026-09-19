1dc98e87dc9b6716f9fef17c704af3def3987450
VyayKumar <49907198+DavidusRidero@users.noreply.github.com>
2026-09-19 14:16:43 +0530

    chore: Corrections

--- Q/Q21-30/Q26/Q26.c
+++ Q/Q21-30/Q26/Q26.c
@@ -1,11 +1,10 @@
 #include <unistd.h>
 
 int main(void) {
-    char *const args[] = {"/bin/ls", "-l", NULL};
-
+    char *const args[] = {"Q25", NULL};
     char *const envp[] = {NULL};
 
-    execve("/bin/ls", args, envp);
+    execve("../Q25/Q25", args, envp);
 
     write(STDERR_FILENO, "execve failed\n", 14);
     _exit(1);
--- Q/Q21-30/Q27/Q27.c
+++ Q/Q21-30/Q27/Q27.c
@@ -1,16 +1,55 @@
 #include <unistd.h>
+#include <sys/wait.h>
 
-int main(int argc, char *argv[]) {
-    // TODO: Demonstrate execl("/bin/ls", "ls", "-Rl", NULL).
-    execl("/bin/ls", "ls", "-Rl", NULL);
+int main(void) {
+    pid_t pid;
+    char *const envp[] = {NULL};
+    char *const args[] = {"ls", "-Rl", NULL};
 
-    // TODO: Demonstrate execlp("ls", "ls", "-Rl", NULL).
-    execlp("ls", "ls", "-Rl", NULL);
+    // a. execl: path, argument list, NULL
+    pid = fork();
+    if (pid == 0) {
+        execl("/bin/ls", "ls", "-Rl", NULL);
+        write(STDERR_FILENO, "execl failed\n", 13);
+        _exit(1);
+    }
+    wait(NULL);
 
-    // TODO: Demonstrate execv("/bin/ls", argv)
-    execv("/bin/ls", argv);
+    // b. execlp: filename, argument list, NULL (searches PATH)
+    pid = fork();
+    if (pid == 0) {
+        execlp("ls", "ls", "-Rl", NULL);
+        write(STDERR_FILENO, "execlp failed\n", 14);
+        _exit(1);
+    }
+    wait(NULL);
 
-    // TODO: Handle failure if exec returns.
-    write(STDOUT_FILENO, "exec() failed", 13);
-    _exit(1);
+    // c. execle: path, argument list, NULL, environment array
+    pid = fork();
+    if (pid == 0) {
+        execle("/bin/ls", "ls", "-Rl", NULL, envp);
+        write(STDERR_FILENO, "execle failed\n", 14);
+        _exit(1);
+    }
+    wait(NULL);
+
+    // d. execv: path, argument array
+    pid = fork();
+    if (pid == 0) {
+        execv("/bin/ls", args);
+        write(STDERR_FILENO, "execv failed\n", 13);
+        _exit(1);
+    }
+    wait(NULL);
+
+    // e. execvp: filename, argument array (searches PATH)
+    pid = fork();
+    if (pid == 0) {
+        execvp("ls", args);
+        write(STDERR_FILENO, "execvp failed\n", 14);
+        _exit(1);
+    }
+    wait(NULL);
+
+    return 0;
 }
