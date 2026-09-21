a2c9089881071c9ea9af5a37c6582987213e8771
VyayKumar <49907198+DavidusRidero@users.noreply.github.com>
2026-09-21 17:36:55 +0530

    [Q2] feat: writer completed.

--- Q/Q1-10/Q2/Q_2_writer.c
+++ Q/Q1-10/Q2/Q_2_writer.c
@@ -12,16 +12,12 @@
 #define FIFO_PATH "my_pipe"
 
 int main(void) {
-    // TODO: Call mkfifo with the path and file permission bits (e.g. 0666)
     int res = mkfifo(FIFO_PATH, 0666);
     if (res == -1 && errno != EEXIST) {
         perror("mkfifo");
         exit(1);
     }
 
-
-    // TODO: Open the named pipe with the appropriate access mode (e.g. O_WRONLY or O_RDONLY)
-    // TODO: If the file descriptor is negative, handle the open error and exit
     printf("Opening pipe for writing (waiting for a reader)...\n");
     int fd = open(FIFO_PATH, O_WRONLY);
     if (fd < 0) {
@@ -29,7 +25,6 @@ int main(void) {
         exit(1);
     }
 
-    // TODO: Write to or read from the file descriptor using write() or read()
     const char *msg = "Hello through FIFO!\n";
     ssize_t bytes_written = write(fd, msg, strlen(msg));
     if (bytes_written == -1) {
@@ -39,13 +34,11 @@ int main(void) {
     }
     printf("Sent %zd bytes.\n", bytes_written);
 
-    // TODO: Close the file descriptor using close()
     if (close(fd) == -1) {
         perror("close");
         exit(1);
     }
 
-    // TODO: If cleanup is required, remove the named pipe from the filesystem using unlink()
     if (unlink(FIFO_PATH) == -1) {
         perror("unlink");
         exit(1);
