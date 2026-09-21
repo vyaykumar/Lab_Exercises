d72da28a72281cc4f46875ec594182dd9905b43d
VyayKumar <49907198+DavidusRidero@users.noreply.github.com>
2026-09-21 19:21:59 +0530

    [Q26] feat: complete.

--- Q/Q21-30/Q_26/Q_26.c
+++ Q/Q21-30/Q_26/Q_26.c
@@ -1,10 +1,10 @@
 #include <unistd.h>
 
 int main(void) {
-    char *const args[] = {"Q25", NULL};
+    char *const args[] = {"Q_25", NULL};
     char *const envp[] = {NULL};
 
-    execve("../Q25/Q25", args, envp);
+    execve("../Q_25/Q_25", args, envp);
 
     write(STDERR_FILENO, "execve failed\n", 14);
     _exit(1);
